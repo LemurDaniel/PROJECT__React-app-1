e37b60290d7ac509223defefcf8c916ac2d841d3
Daniel Landau <49441790+LemurDaniel@users.noreply.github.com>
2021-08-22 16:09:43 +0200

    Update renew.sh

--- docker/deploy/ubuntuConfig/renew.sh
+++ docker/deploy/ubuntuConfig/renew.sh
@@ -2,23 +2,32 @@
 
 file=$(cat /root/PROJECT__React-app-1/docker/deploy/ubuntuConfig/lastCertDate.txt)
 
-echo $(date -d $file '+%Y/%m/%d')
-echo $(date -d '80 days ago' '+%Y/%m/%d')
+lastDateFormated=$(date -d $file +%Y/%m/%d)
+expirationFormated=$(date -d '80 days ago' +%Y/%m/%d)
 
 lastDate=$(date -d $file '+%s')
 expiration=$(date -d '80 days ago' +%s)
 
+currentDate=$(date -d 'today' '+%Y/%m/%d')
+currentLog=$currentDate'/ --- expiration: '$expirationFormated' - lastDate: '$lastDateFormated' || ['$expiration' -ge '$lastDate'] ===> '
+
 if [ $expiration -ge $lastDate ];
 then
     echo 'YES'
+
     docker-compose -f /root/PROJECT__React-app-1/docker/deploy/dc-web-db.yaml stop
 
     docker-compose -f /root/PROJECT__React-app-1/docker/deploy/certbot.yaml up
     docker-compose -f /root/PROJECT__React-app-1/docker/deploy/certbot.yaml stop
 
+    currentLog=$currentLog' YES '
     echo $(date '+%Y/%m/%d') > /root/PROJECT__React-app-1/docker/deploy/ubuntuConfig/lastCertDate.txt
 else
     echo 'NO'
+    currentLog=$currentLog' NO '
 fi  
+
+echo $currentLog
+echo $currentLog >> /root/PROJECT__React-app-1/docker/deploy/ubuntuConfig/log.txt
 
 docker-compose -f /root/PROJECT__React-app-1/docker/deploy/dc-web-db.yaml up -d
